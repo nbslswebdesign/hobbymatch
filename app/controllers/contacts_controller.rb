7ab82c8d48e8374628b2c97a9038d8a1cb465055
Nick Smith <theteam@leedswebsite.com>
2019-09-17 17:41:43 +0000

    Added action for contact to send message to database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,21 @@
 class ContactsController < ApplicationController
 
     def new
+        @contact = Contact.new
     end
+    
+    def create
+       @contact = Contact.new(contact_params)
+       if @contact.save 
+           redirect_to new_contact_path, notice: "We got your message, thanks."
+       else 
+           redirect_to new_contact_path, notice: "Ooops, sorry an error occured. Please try again."
+       end
+    end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
     
 end

7c7c7e40b77f5b89bf14f6c6b48b4a72ffe1b47a
Nick Smith <theteam@leedswebsite.com>
2019-09-18 12:07:59 +0000

    Added elements to the mailer page

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,9 @@
 class ContactMailer < ActionMailer::Base
-    default to : 'theteam@leedswebsite.com'
-    
-    def contact_email(name, email, body)
-       @name = name
-       @email = email
-       @body = body
-       
-       mail(from: email, subject: 'You have a new message')
-    end
+  default to: 'theteam@leedswebsite.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'You have a new message')
+  end
 end

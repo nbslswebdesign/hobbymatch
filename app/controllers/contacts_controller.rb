ff32b5dc3e29995bdc2d74442b735d9ca21eb886
Nick Smith <theteam@leedswebsite.com>
2019-09-25 07:50:24 +0000

    Install Devise Gem for user authentication

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,34 @@
 class ContactsController < ApplicationController
 
+    # GET request to /contact-us
+    # Show new contact form
     def new
         @contact = Contact.new
     end
     
-    def create
+    #POST request to /contacts
+        def create
+        # Mass assignment of form fields into Contact objects
        @contact = Contact.new(contact_params)
        if @contact.save 
+           # Store form fields via parameters , in to variables
            name = params[:contact][:name]
            email = params[:contact][:email]
            body = params[:contact][:comments]
+           # Plug variables in to the ContactMailer email method and send email
            ContactMailer.contact_email(name, email, body).deliver
+           # Store success message in flash hash and redirect to new action
            flash[:success] = "We got your message, thank you"
            redirect_to new_contact_path
        else 
+           # If object doesnt save store errors in flash hash and redirect to new action
            flash[:danger] = @contact.errors.full_messages.join(", ")
            redirect_to new_contact_path
        end
-    end
+        end
     
     private
+    # To collect data from form, we need to use strong parameters and whitelist form fields
         def contact_params
             params.require(:contact).permit(:name, :email, :comments)
         end

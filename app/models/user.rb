626009491d7ff0c05957a1c6635077ac235943e6
Nick Smith <theteam@leedswebsite.com>
2019-10-04 09:17:51 +0000

    Added new user details and more

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ attr_accessor :stripe_card_token
 def save_with_subscription
   if valid?
     customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
-      self.stripe_customer_token = customer.id
-      save!
+    self.stripe_customer_token = customer.id
+    save!
   end
 end
 end

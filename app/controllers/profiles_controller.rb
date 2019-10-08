113b36118b7efbe0156a5e7ed151f8a81d95ee0d
Nick Smith <theteam@leedswebsite.com>
2019-10-08 10:32:17 +0000

    Added user profiles and css styling sheet

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,21 +6,21 @@ class ProfilesController < ApplicationController
   end
   
   # POST to /users/:user_id/profile
-def create
+  def create
   # Ensure that we have the user who is filling out form
   @user = User.find( params[:user_id] )
   # Create profile linked to this specific user
   @profile = @user.build_profile( profile_params )
-  if @profile.save
-    flash[:success] = "Profile updated!"
-    redirect_to user_path( params[:user_id] )
-  else
-    render action: :new
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :new
+    end
   end
-end
-private
-  def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :contact_email, :phone_number, :description, :hobbies)
+    private
+      def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :avatar, :contact_email, :phone_number, :description, :hobbies)
   end
   
 end
